83e8fa82959681dcabf03d958c5fa6699e3d471c
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-21 07:46:21 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -24,25 +24,21 @@ def tempConverter():
     firstu = str(input("Please enter the the first measuring unit to convert from one to another (C/F/K) > "))
     secondu = str(input("Please enter the the second measuring unit to convert from one to another (C/F/K) > "))
     if firstu.capitalize() == "C" and secondu.capitalize() == "F":
-        print("You have chosen to convert from Celsius to Fahrenheit")
         c2f= input("Please enter the temperature to convert from C to F > ")
         if c2f.isnumeric():
             c2f = int(c2f)
             print("The temperature in Fahrenheit is", (c2f * 9/5) + 32)
     elif firstu.capitalize() == "F" and secondu.capitalize() == "C":
-        print("You have chosen to convert from Fahrenheit to Celsius")
         f2c = input("Please enter the temperature to convert from F to C > ")
         if f2c.isnumeric():
             f2c = int(f2c)
             print("The temperature in Celsius is", (f2c - 32) * 5/9)
     elif firstu.capitalize() == "K" and secondu.capitalize() == "C":
-        print("You have chosen to convert from Kelvin to Celsius")
         k2c= input("Please enter the temperature to convert from K to C > ")
         if k2c.isnumeric():
             k2c = int(k2c)
             print("The temperature in Celsius is", k2c - 273.15)
     elif firstu.capitalize() == "C" and secondu.capitalize() == "K":
-        print("You have chosen to convert from Celsius to Kelvin")
         c2k= input("Please enter the temperature to convert from C to K > ")
         if c2k.isnumeric():
             c2k = int(c2k)
@@ -53,16 +49,15 @@ def tempConverter():
 def bmiCalculator():
     w = float(input("Please enter your weight in kilograms > "))
     h = float(input("Please enter your height in meters > "))
-    if w == 0:
-        print("Please enter a valid weight as that is not physically possible")
-    elif h == 0:
-        print("Please enter a valid height as that is not physically possible")
+    if w == 0 and h == 0:
+        print("Please enter a valid weight or height as that is not physically possible")
     else:
         print("Your BMI is ", w/(h**2))
 
 def exit():
     print("Thank you for using the program")
     print("Exiting...")
+    print("Exited")
     
 
 def main():
@@ -73,13 +68,25 @@ def main():
     print("Press 4 to quit")
     print("**********************")
     choice = int(input("Please enter your choice > "))
-    if choice == 1:
-        calculator()
-    elif choice == 2:
-        bmiCalculator()
-    elif choice == 3:
-        tempConverter()
-    elif choice == 4:
-        exit()
-    else:
-        print("Please enter a valid choice")
+    while(choice != 4):
+        if choice == 1:
+            calculator()
+            main()
+        elif choice == 2:
+            bmiCalculator()
+            main()
+        elif choice == 3:
+            tempConverter()
+            main()
+        else:
+            print("Please enter a valid choice")
+        print("**********************")
+        print("Press 1 to use calculator \n")
+        print("Press 2 to use BMI calculator \n")
+        print("Press 3 to use temperature converter \n")
+        print("Press 4 to quit")
+        print("**********************")
+        choice = int(input("Please enter your choice > "))
+    exit()
+
+main()
